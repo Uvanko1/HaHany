b02f952065525be7e31740ef785190ea2183b414
vinndim <vinnikov.dima2005@gmail.com>
2021-12-20 21:25:15 +0500

    up fps

--- constans.py
+++ constans.py
@@ -3,5 +3,5 @@ MAP_FILE = 'mongolia.tmx'
 size = [800, 800]
 WINDOW_SIZE = 800, 800
 GAME_NAME = 'Чингисхан'
-
+res = [400, 400]
 cam_x, cam_y = 0, 0
--- main.py
+++ main.py
@@ -1,56 +1,92 @@
 import pygame
-import pytmx
 
+import pytmx
 from constans import *
+
+pygame.init()
+
+size = [800, 800]
+res = [400, 400]
+
+cam_x, cam_y = 0, 0
+
+window = pygame.display.set_mode(size)
+screen = pygame.transform.scale(window, res)
+
+clock = pygame.time.Clock()
+
+chunk_size = 8
+world_map = pytmx.load_pygame(f'{MAPS_DIR}/{MAP_FILE}')
+tile_size = world_map.tilewidth
+textures = None
+
+world_size_chunk_x = 1024 // chunk_size
+world_size_chunk_y = 1024 // chunk_size
+
+
+def chunks_on_screen():
+    x1 = cam_x // (chunk_size * tile_size)
+    y1 = cam_y // (chunk_size * tile_size)
+
+    x2 = (cam_x + res[0]) // (chunk_size * tile_size)
+    y2 = (cam_y + res[1]) // (chunk_size * tile_size)
+
+    x1 = min(max(x1, 0), world_size_chunk_x - 1)
+    x2 = min(max(x2, 0), world_size_chunk_x - 1)
+
+    y1 = min(max(y1, 0), world_size_chunk_y - 1)
+    y2 = min(max(y2, 0), world_size_chunk_y - 1)
+
+    result = []
+    for y in range(y1, y2 + 1):
+        for x in range(x1, x2 + 1):
+            result.append(x + y * world_size_chunk_x)
+
+    return result
 
 
 class Chunk:
-    def __init__(self, filename_map):
-        self.map = pytmx.load_pygame(f'{MAPS_DIR}/{filename_map}')
-        self.height = self.map.height
-        self.width = self.map.width
-        self.tile_size = self.map.tilewidth
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.map = world_map
 
-    def render(self, screen, cam_x, cam_y):
-        for y in range(self.height):
-            for x in range(self.width):
-                image = self.map.get_tile_image(x, y, 0)
-                print(cam_x, cam_y)
-                screen.blit(image, (x * self.tile_size - cam_x, y * self.tile_size - cam_y))
-
-    def get_title_id(self, position):
-        return self.map.tiledgidmap[self.map.get_tile_gid(*position, 0)]
+    def render(self):
+        for y in range(chunk_size):
+            for x in range(chunk_size):
+                texture = self.map.get_tile_image(x, y, 0)
+                screen.blit(texture, (self.x + x * tile_size - cam_x, self.y + y * tile_size - cam_y))
 
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode(WINDOW_SIZE)
-    chunk = Chunk(MAP_FILE)
-    clock = pygame.time.Clock()
-    cam_x, cam_y = 0, 0
-    frame = 0
-    while 1:
-        screen.fill((0, 0, 0))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
-            key = pygame.key.get_pressed()
-            if key[pygame.K_a]:
-                cam_x -= 10
-            if key[pygame.K_d]:
-                cam_x += 10
-            if key[pygame.K_w]:
-                cam_y -= 10
-            if key[pygame.K_s]:
-                cam_y += 10
+chunks = []
+for y in range(world_size_chunk_y):
+    for x in range(world_size_chunk_x):
+        chunks.append(Chunk(x * chunk_size * tile_size, y * chunk_size * tile_size))
 
-        chunk.render(screen, cam_x, cam_y)
-        pygame.display.update()
-        clock.tick(480)
-        frame += 1
-        if frame % 10 == 0:
-            pygame.display.set_caption('FPS' + str(round(clock.get_fps())))
+frame = 0
+while 1:
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
 
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a]:
+        cam_x -= 1
+    if key[pygame.K_d]:
+        cam_x += 1
+    if key[pygame.K_w]:
+        cam_y -= 1
+    if key[pygame.K_s]:
+        cam_y += 1
 
-if __name__ == "__main__":
-    main()
+    for i in chunks_on_screen():
+        chunks[i].render()
+
+    window.blit(pygame.transform.scale(screen, size), (0, 0))
+    pygame.display.update()
+    clock.tick(480)
+
+    frame += 1
+    if frame % 100 == 0:
+        pygame.display.set_caption('FPS: ' + str(round(clock.get_fps())))
+        chunks_on_screen()
