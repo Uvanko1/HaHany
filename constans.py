cf6e15a4da2741e0172abe2716d86c3e559fd82f
vinndim <vinnikov.dima2005@gmail.com>
2021-12-24 08:23:18 +0500

    deleted shit

--- constans.py
+++
@@ -1,8 +0,0 @@
-MAPS_DIR = 'maps'
-MAP_FILE = 'безымянный.tmx'
-size = [1920, 1060]
-WINDOW_SIZE = 1920, 1080
-GAME_NAME = 'Чингисхан'
-res = [400, 400]
-title_size = 32
-cam_x, cam_y = 0, 0
--- maps/mountain_landscape_25.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="mountain_landscape_25" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="mountain_landscape_25.png" width="512" height="512"/>
-</tileset>
